178764900490e71a2a735419171f8ddc1c7b185c
Batoolaelgohary <batoolelgohary@gmail.com>
2024-06-09 00:03:09 +0300

    update

--- js/Auth.js
+++ js/Auth.js
@@ -1,3 +1,3 @@
-if (localStorage.getItem('token') === null || undefined) {
-    location.href ='../index.html'
+if (localStorage.getItem('token') === null ) {
+    location.href = './index.html';
 }
